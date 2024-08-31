621f2b48dc91d8f605a63fd10416bde7a6c86f84
alishkarki <129705823+aliskarkee@users.noreply.github.com>
2024-08-31 11:52:46 +1000

    qn2.js

--- MODULE3/Intermediate/qn2.js
+++ MODULE3/Intermediate/qn2.js
@@ -3,5 +3,26 @@
 // ellipsis (...) added to the end if it was too long, or the original text otherwise.
 // b) Write another variant of the truncate function that uses a conditional operator.
 
-console.log(truncate('This text will be truncated if it is too long', 25))
-// This text will be truncat...
+// console.log(truncate('This text will be truncated if it is too long', 25))
+// // This text will be truncat...
+
+// str ="the most beautiful eyes i have ever seen ";
+// max =  10; 
+
+//CREATE A FUNCTION WITH STR AND MAX 
+//node 
+
+function turncate(str, max) {
+    if (str.length > max) {
+        return str.slice(0, max) + '...';
+    } else {
+        return str;
+    }
+}
+/* for b */
+// function turncate(str, max) {
+//     return str.length > max ? str.slice(0, max) + '...' : str;
+// }
+
+
+console.log(turncate('This text will be truncated if its too long', 25));
